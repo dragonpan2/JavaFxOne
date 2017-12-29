1c1838a2f5dbecba5902da6c47f4a7e56409a05e
dragonpan2 <panbeining@hotmail.com>
2017-12-29 18:16:36 -0500

    update on admin pane

--- JavaFXApplication1/src/javafxapplication1/JavaFXApplication1.java
+++ JavaFXApplication1/src/javafxapplication1/JavaFXApplication1.java
@@ -5,6 +5,7 @@
  */
 package javafxapplication1;
 
+import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -12,6 +13,8 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
@@ -43,6 +46,9 @@ public class JavaFXApplication1 extends Application {
         StackPane root = new StackPane();
         Pane paneOperation = new Pane();
         Pane paneAdmin = new Pane();
+        
+        Pane selection = new Pane();
+        selection.setVisible(false);
 
         hboxTop.getChildren().addAll(btnOperation, btnStatistic, btnManagement, btnExpansion);
         hboxTop.setAlignment(Pos.CENTER);
@@ -207,9 +213,90 @@ public class JavaFXApplication1 extends Application {
         Scene scene = new Scene(root, 1000, 700);
     //    scene.setFill(Color.DARKGREY);
         
+         ///-------------------------------------------------------------------------------------------------------------------------------------------------------
+         //Admin tab
+         ///login tab
+         Pane loginPane = new Pane();
+         PasswordField passField = new PasswordField();
+         Label passLabel = new Label("Passcode");
+         Button passBtn = new Button("Confirm");
          
-        
-        adminTabIni(paneAdmin);
+         passLabel.setLayoutX(300);
+         passField.setLayoutX(400);
+         passBtn.setLayoutY(50);
+         passBtn.setLayoutX(400);
+         
+         loginPane.getChildren().addAll(passField, passLabel, passBtn);
+         paneAdmin.getChildren().add(loginPane);
+         passBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if (passField.getText().equals("8462")) {
+                    loginPane.setVisible(false);
+                    selection.setVisible(true);
+                    passField.clear();
+                }
+            }
+        });
+         ///seletion menu
+         
+         VBox selectionVB = new VBox();
+         Button addProductBtn = new Button("Add new Product");
+         Button addUserBtn = new Button("Add new User");
+         Button manageProductBtn = new Button("Manage Inventory");
+         Button manageUserBtn = new Button("Manage Users");
+         Button logoutBtn = new Button("Logout");
+         
+         Pane newUserPane = new Pane();
+         Pane newProductPane = new Pane();
+         iniNewProduct(newProductPane, selection);
+         paneAdmin.getChildren().add(newProductPane);
+         Pane manageProductPane = new Pane();
+         iniManageProduct(manageProductPane, selection);
+         paneAdmin.getChildren().add(manageProductPane);
+         iniNewUser(newUserPane, selection);
+         paneAdmin.getChildren().add(newUserPane);
+         
+         addProductBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                newProductPane.setVisible(true);
+                    selection.setVisible(false);
+                
+            }
+        });
+         addUserBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                  selection.setVisible(false);
+                  newUserPane.setVisible(true);
+            }
+        });
+         manageProductBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                  selection.setVisible(false);
+                   System.out.println("Open manager");
+                  manageProductPane.setVisible(true);
+            }
+        });
+         manageUserBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                  selection.setVisible(false);
+            }
+        });
+         logoutBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                 selection.setVisible(false);
+                 loginPane.setVisible(true);
+            }
+        });
+         selectionVB.getChildren().addAll(addProductBtn,addUserBtn,manageProductBtn,manageUserBtn, logoutBtn);
+         selection.getChildren().add(selectionVB);
+         paneAdmin.getChildren().add(selection);
+        ///-----------------------------------------------------------------------------------------------------------------------------------------------------
         //  stage >scene > root > component 
         primaryStage.setTitle("PolyPhoto Inventory Management v0.1");
         primaryStage.setScene(scene);
@@ -225,9 +312,9 @@ public class JavaFXApplication1 extends Application {
         launch(args);
     }
     
-    public  void adminTabIni(Pane paneAdmin) {
-        /// create new user
-        /// create new product
+    public  void iniNewProduct(Pane newProductPane, Pane selection) {
+     
+        
         Label lblProductCode = new Label("Product Code Bar");
         Label lblProductName = new Label("Name");
         Label lblProductPrice = new Label("Price");
@@ -248,23 +335,174 @@ public class JavaFXApplication1 extends Application {
         newProductGrid.add(TFProductName, 1, 1);
         newProductGrid.add(TFProductPrice, 1, 2);
         newProductGrid.add(TFProductQuantity, 1, 3);
-        paneAdmin.getChildren().add(newProductGrid);
-   //    GridPane.setStyle("-fx-background-color:#2c3e50;");
         
-        /// manager user
-        ///manage inventory
+       newProductGrid.setStyle("-fx-background-color:#2c3e50;");
+        newProductPane.setVisible(false);
+        
+        Button createButton = new Button("Create New Product");
+        Button backButton = new Button("Return to previous menu");
+        
+        createButton.setLayoutX(50);
+        createButton.setLayoutY(50);
+        backButton.setLayoutX(50);
+        backButton.setLayoutY(100);
+        newProductPane.getChildren().addAll(newProductGrid, createButton,backButton);
+        
+        createButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+              //  Main.firstTimeProtocol();
+            ProductList productList = new ProductList();
+            productList.productList = new ArrayList<Product>();
+            Main.readProductDataFile(productList);
+            Product product = new Product();
+            product.setProductCode(TFProductCode.getText());
+            product.setProductName(TFProductName.getText());
+            product.setPrice(Integer.parseInt(TFProductPrice.getText()));
+            product.setQuantityLeft(Integer.parseInt(TFProductQuantity.getText()));
+            productList.productList.add(product);
+            Main.writeProdctDataFile(productList);
+            }
+        });
+        
+        backButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                newProductPane.setVisible(false);
+                selection.setVisible(true);
+            }
+        });
+        
+        
+    }
+    public void iniNewUser(Pane newUserPane, Pane selection) {
+         Label lblUserID = new Label("Card Bar Code");
+         Label lblUserName = new Label("UserName");
+         Label lblUserFund = new Label("Fund");
+         TextField TFUserID = new TextField("0");
+         TextField TFUserName = new TextField("-");
+         TextField TFUserFund = new TextField("0");
+         
+           GridPane newUserGrid = new GridPane();
+        
+        newUserGrid.add(lblUserID, 0, 0);
+        newUserGrid.add(lblUserName, 0, 1);
+        newUserGrid.add(lblUserFund, 0, 2);
+        
+        newUserGrid.add(TFUserID, 1, 0);
+        newUserGrid.add(TFUserName, 1, 1);
+        newUserGrid.add(TFUserFund, 1, 2);
+        
+       newUserGrid.setStyle("-fx-background-color:#2c3e50;");
+        newUserPane.setVisible(false);
+        
+        Button createButton = new Button("Create New User");
+        Button backButton = new Button("Return to previous menu");
+        
+        createButton.setLayoutX(150);
+        createButton.setLayoutY(100);
+        backButton.setLayoutX(150);
+        backButton.setLayoutY(150);
+        newUserPane.getChildren().addAll(newUserGrid, createButton,backButton);
+        
+        createButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+              //  Main.firstTimeProtocol();
+            UserList userListMaster = new UserList();
+            userListMaster.userList = new ArrayList<User>();
+            Main.readUserDataFile(userListMaster);
+            User user = new User();
+            
+            user.setCardId(TFUserID.getText());
+            user.setUsername(TFUserName.getText());
+            user.setBalance(Integer.parseInt(TFUserFund.getText())); //careful now
+            userListMaster.userList.add(user);
+            Main.writeUserDataFile(userListMaster);
+            }
+        });
+        
+        backButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                newUserPane.setVisible(false);
+                selection.setVisible(true);
+            }
+        });
+        
+    }
+    public void iniManageProduct(Pane manageProductPane, Pane selection) {
+        manageProductPane.setVisible(false);
+        ScrollPane productScrollPane = new ScrollPane();
+        ProductList masterProductList = new ProductList();
+        masterProductList.productList = new ArrayList<Product>();
+        Main.readProductDataFile(masterProductList);
+        
+        Button backButton = new Button("Return");
+        manageProductPane.getChildren().add(backButton);
+        backButton.setLayoutX(400);
+        backButton.setLayoutY(50);
+        backButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                  selection.setVisible(true);
+                  manageProductPane.setVisible(false);
+            }
+        });
+        
+        for (int i = 0; i < masterProductList.productList.size(); i++) {
+            
+           
+            
+        HBox hBoxOne = new HBox();
+        VBox vboxLabel = new VBox();
+        vboxLabel.setSpacing(10);
+        
+        Label lblProductCode = new Label("Product Code Bar");
+        Label lblProductName = new Label("Name");
+        Label lblProductPrice = new Label("Price");
+        Label lblProductQuantity = new Label("Quantity");
+        lblProductCode.setTextFill(Color.web("#bdc3c7"));
+        lblProductName.setTextFill(Color.web("#bdc3c7"));
+        lblProductPrice.setTextFill(Color.web("#bdc3c7"));
+        lblProductQuantity.setTextFill(Color.web("#bdc3c7"));
+        lblProductCode.setFont(Font.font("Abel",FontWeight.BOLD, 12));
+        lblProductName.setFont(Font.font("Abel",FontWeight.BOLD, 12));
+        lblProductPrice.setFont(Font.font("Abel",FontWeight.BOLD, 12));
+        lblProductQuantity.setFont(Font.font("Abel",FontWeight.BOLD, 12));
+        vboxLabel.getChildren().addAll(lblProductCode, lblProductName, lblProductPrice,lblProductQuantity);
+
+        VBox vboxTF = new VBox();
+        TextField TFProductCode = new TextField("-");
+        TextField TFProductName = new TextField("-");
+        TextField TFProductPrice = new TextField("0");
+        TextField TFProductQuantity = new TextField("0");
+        TFProductCode.setAlignment(Pos.CENTER);
+        TFProductName.setAlignment(Pos.CENTER);
+        TFProductPrice.setAlignment(Pos.CENTER);
+        TFProductQuantity.setAlignment(Pos.CENTER);
+
+        vboxTF.getChildren().addAll(TFProductCode, TFProductName, TFProductPrice,TFProductQuantity);
+        hBoxOne.getChildren().addAll(vboxLabel,vboxTF);
+        hBoxOne.setSpacing(25);
+        hBoxOne.setMaxSize(350, 50);
+        
+        StackPane stackH1 = new StackPane();
+        stackH1.getChildren().add(hBoxOne);
+        stackH1.setStyle("-fx-background-color:#2c3e50;");
+        stackH1.setStyle("-fx-background-color: #34495e;");
+        stackH1.setLayoutX(250);
+        stackH1.setLayoutY(50*(1+i));
+        stackH1.setPrefSize(450, 100);
+        stackH1.setAlignment(hBoxOne,Pos.CENTER);
+        productScrollPane.getChildrenUnmodifiable().add(stackH1);
+        manageProductPane.getChildren().add(productScrollPane);
+        }
+    }
+    public void iniManageUser(Pane manageUserPane, Pane selection) {
         
     }
 
-//    public void keyPressed(KeyEvent e) {
-//
-//        int key = e.getKeyCode();
-//
-//        if (key != KeyEvent.VK_ENTER) {
-//            System.out.println("Triggered");
-//        } else {
-//
-//        }
-//    }
+
 
 }
--- JavaFXApplication1/src/javafxapplication1/Main.java
+++ JavaFXApplication1/src/javafxapplication1/Main.java
@@ -10,6 +10,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
 /**
  *
@@ -59,7 +60,7 @@ public class Main {
             /// next is ready for new operation
         }
     }
-    public void writeUserDataFile (UserList userListMaster) {
+    public static void writeUserDataFile (UserList userListMaster) {
         try {
             
         FileOutputStream fileOut = new FileOutputStream("userData.ser");
@@ -74,9 +75,10 @@ public class Main {
             System.out.println("Exception");
         }
     }
-    public void writeProdctDataFile (ProductList productListMaster) {
+    public static void writeProdctDataFile (ProductList productListMaster) {
+        productListMaster.productList = new ArrayList<Product>();
+        System.out.println("Writing File");
         try {
-            
         FileOutputStream fileOut = new FileOutputStream("productData.ser");
         ObjectOutputStream out = new ObjectOutputStream(fileOut);
         out.writeObject(productListMaster);
@@ -88,11 +90,11 @@ public class Main {
             
             System.out.println("Exception");
         }
+        System.out.println("File Writed");
     }
-    public void readProductDataFile(ProductList productListMaster) {
+    public static void readProductDataFile(ProductList productListMaster) {
+        System.out.println("Reading File");
         try {
-            
-        
         FileInputStream fileIn = new FileInputStream("productData.ser");
         ObjectInputStream in = new ObjectInputStream(fileIn);
         productListMaster = (ProductList) in.readObject();
@@ -105,8 +107,10 @@ public class Main {
         catch (IOException i) {
             System.out.println("Exception");
         }
+        System.out.println("File Read");
+       // System.out.println(productListMaster.productList.get(0).getProductCode());
     }
-    public void readUserDataFile(UserList userListMaster) {
+    public static void readUserDataFile(UserList userListMaster) {
         try {
             
         
@@ -123,7 +127,7 @@ public class Main {
             System.out.println("Exception");
         }
     }
-    public void addNewProduct(ProductList productListMaster, String productCode, String productName, double price, int quantityToAdd) {
+    public static void addNewProduct(ProductList productListMaster, String productCode, String productName, double price, int quantityToAdd) {
         Product newProduct = new Product();
         
         newProduct.setProductCode(productCode);
@@ -133,7 +137,7 @@ public class Main {
         
         productListMaster.productList.add(newProduct);
     }
-    public void addNewProduct(ProductList productListMaster, String productCode, String productName, double price) {
+    public static void addNewProduct(ProductList productListMaster, String productCode, String productName, double price) {
         Product newProduct = new Product();
         newProduct.setProductCode(productCode);
         newProduct.setProductName(productName);
@@ -142,7 +146,7 @@ public class Main {
         
         productListMaster.productList.add(newProduct);
     }
-    public void addNewUser(UserList userListMaster, String idBarcode, String name, double balance ) {
+    public static void addNewUser(UserList userListMaster, String idBarcode, String name, double balance ) {
         User newUser = new User();
         newUser.setCardId(idBarcode);
         newUser.setUsername(name);
@@ -159,5 +163,21 @@ public class Main {
     }
     public void productAdjustQuantity(Product product,int quantityAdjust) {
         product.setQuantityLeft(product.getQuantityLeft()+quantityAdjust);
+    }
+    public static void firstTimeProtocol() {
+        System.out.println("Executing First Time Protocol");
+        try {
+        FileOutputStream fileOut = new FileOutputStream("productData.ser");
+        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+        out.writeObject(new ProductList());
+        out.close();
+        fileOut.close();
+        }
+        
+        catch (IOException i) {
+            
+            System.out.println("Exception");
+        }
+        System.out.println("Done");
     }
 }
